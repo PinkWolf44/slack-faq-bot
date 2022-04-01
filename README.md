9c73c4f959a2eeaf5d4d708056408aa723f42ec7
Hector Hurtado <hhurtado@sonatype.com>
2022-04-01 09:16:41 -0500

    Adding modal to store a new knowledge base entry

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # slack-faq-bot
 Slack bot that helps to create a Questions &amp; Answers database and helps to automatically answer to 
 frequently asked questions
+
+## How to run
+
+### Prepare the environment
+Before running the app you need to follow the next steps to prepare the environment:
+
+```bash
+# For Linux
+python3 -m venv .venv
+source .venv/bin/activate
+export SLACK_BOT_TOKEN=xapp-<your-app-token>
+export SLACK_APP_TOKEN=xoxb-<your-bot-token>
+pip install slack_bolt
+
+# For Windows
+py -m venv env
+.\env\Scripts\activate
+set SLACK_BOT_TOKEN=xapp-<your-app-token>
+set SLACK_APP_TOKEN=xoxb-<your-bot-token>
+pip install slack_bolt
+```
+
+### Run the app
+You will need to execute the next command:
+```bash
+# For Linux
+python3 app.py
+
+# For Windows
+py app.py
+```
+
+## Docs
+Slack docs: https://api.slack.com/docs
+Bolt SDKs: https://api.slack.com/tools/bolt
+Bolt Python SDK: https://slack.dev/bolt-python/concepts
--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import pandas as pd
+import json
 
 
 # Install the Slack app and get xoxb- token in advance
@@ -20,35 +22,168 @@ def event_test(event, say):
 def open_modal(ack, shortcut, client, body):
     # Acknowledge the shortcut request
     ack()
-    print('Body: ',body)
+
+    metadata = body["channel"]["id"] + "-" + body["message_ts"] + "-" + body["message"]["thread_ts"]
+    solution = body["message"]["text"]
+
+    result = client.conversations_history(
+        channel=body["channel"]["id"],
+        inclusive=True,
+        oldest=body["message"]["thread_ts"],
+        limit=1
+    )
+
+    problem = result["messages"][0]["text"]    
+    
     # Call the views_open method using the built-in WebClient
-    client.views_open(
+    result = client.views_open(
         trigger_id=shortcut["trigger_id"],
+        
         # A simple view payload for a modal
-        view={
+        view = {
             "type": "modal",
-            "title": {"type": "plain_text", "text": "My App"},
-            "close": {"type": "plain_text", "text": "Close"},
+            "title": {
+                "type": "plain_text",
+                "text": "New Knowladge Base Entry",
+                "emoji": True
+            },
+            "private_metadata": metadata,
+            "submit": {
+                "type": "plain_text",
+                "text": "Submit",
+                "emoji": True
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Cancel",
+                "emoji": True
+            },
             "blocks": [
                 {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+                    "block_id": "product",
+                    "type": "input",
+                    "element": {
+                        "type": "static_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select a Product",
+                            "emoji": True
+                        },
+                        "options": [
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Nexus IQ CLI",
+                                    "emoji": True
+                                },
+                                "value": "value-0"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Nexus IQ Jenkins Plugin",
+                                    "emoji": True
+                                },
+                                "value": "value-1"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Nexus IQ Jira Plugin",
+                                    "emoji": True
+                                },
+                                "value": "value-2"
+                            }
+                        ],
+                        "action_id": "static_select-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Integrations Product",
+                        "emoji": True
                     }
                 },
                 {
-                    "type": "context",
-                    "elements": [
-                        {
-                            "type": "mrkdwn",
-                            "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
-                        }
-                    ]
+                    "type": "divider"
+                },
+                {
+                    "block_id": "problem",
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "multiline": True,
+                        "action_id": "plain_text_input-action",
+                        "initial_value": problem
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Problem / Question",
+                        "emoji": True
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "block_id": "solution",
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "multiline": True,
+                        "action_id": "plain_text_input-action",
+                        "initial_value": solution
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Solution / Response",
+                        "emoji": True
+                    }
                 }
             ]
         }
     )
+
+@app.view("")
+def handle_view_events(ack, client, body):
+    ack()
+    
+    metadata = body["view"]["private_metadata"].split("-")
+    selected_product = body["view"]["state"]["values"]["product"]["static_select-action"]["selected_option"]["value"]
+    problem_question = body["view"]["state"]["values"]["problem"]["plain_text_input-action"]["value"]
+    solution_response = body["view"]["state"]["values"]["solution"]["plain_text_input-action"]["value"]
+    channel = metadata[0]
+    message_ts = metadata[1]
+    thread_ts = metadata[2]
+    
+    response = client.chat_getPermalink(
+        channel=channel,
+        message_ts=message_ts
+    )
+    message_link = response.data["permalink"]
+
+    response = client.chat_getPermalink(
+        channel=channel,
+        message_ts=thread_ts
+    )
+    thread_link = response.data["permalink"]
+
+    entry = [{
+        "Channel": channel,
+        "Message Id": message_ts,
+        "Thread Id": thread_ts,
+        "Product": selected_product,
+        "Problem/Question": problem_question,
+        "Solution/Response": solution_response,
+        "Message Link": message_link,
+        "Thread Link": thread_link
+    }]
+    
+    print('Result: ', entry)
+
+    df = pd.read_csv('data.csv', sep='|')
+    row = pd.DataFrame(entry)
+    df = pd.concat([df, row])
+    df.to_csv('data.csv', encoding='utf-8', sep='|', index=False)
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
